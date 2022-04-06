65d3f62a3cfaa9c3ac52b65b5333515995ef563f
delhen <hendardidel@gmail.com>
2022-04-06 23:15:13 +0700

    Modify Page and Root App

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Layout from '../components/Layout'
+import Body from '../components/Body'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Body>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Body>
+
+  )
 }
 
 export default MyApp
